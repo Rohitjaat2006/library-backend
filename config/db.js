c383813b107fa8e31d64ae46ec1736c10d25114b
Rohitjaat2006 <r126jaat@gmail.com>
2026-03-19 22:03:20 +0530

    use connection pool

--- config/db.js
+++ config/db.js
@@ -1,20 +1,16 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    connectTimeout: 10000 // VERY IMPORTANT
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error("❌ DB Error:", err);
-    } else {
-        console.log("✅ MySQL Connected");
-    }
-});
+console.log("Using connection pool");
 
 module.exports = db;
