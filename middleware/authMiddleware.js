4721173b92f79a15b271bf23396033663f4907e4
Rohitjaat2006 <r126jaat@gmail.com>
2026-03-19 23:30:25 +0530

    fix bearer token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization;
+    let token = req.headers.authorization;
 
     if (!token) {
         return res.status(401).json({ message: "No token provided" });
     }
 
+    // ✅ REMOVE "Bearer "
+    if (token.startsWith("Bearer ")) {
+        token = token.split(" ")[1];
+    }
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // store user info
+        req.user = decoded;
         next();
     } catch (err) {
         return res.status(400).json({ message: "Invalid token" });
